a55b11745c08888edba1e4d3e0ccd7d345fa6e4a
ymii89 <francoisym3@gmail.com>
2020-10-30 10:22:57 +0100

    Le formulaire d'enregistrement du candidat

--- js/main.js
+++ js/main.js
@@ -1,59 +1,35 @@
+$(window, document, undefined).ready(function() {
 
-(function ($) {
-    "use strict";
+  $('input').blur(function() {
+    var $this = $(this);
+    if ($this.val())
+      $this.addClass('used');
+    else
+      $this.removeClass('used');
+  });
 
+  var $ripples = $('.ripples');
 
+  $ripples.on('click.Ripples', function(e) {
 
-  
-  
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
+    var $this = $(this);
+    var $offset = $this.parent().offset();
+    var $circle = $this.find('.ripplesCircle');
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+    var x = e.pageX - $offset.left;
+    var y = e.pageY - $offset.top;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
+    $circle.css({
+      top: y + 'px',
+      left: x + 'px'
     });
 
+    $this.addClass('is-active');
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
+  });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
+  $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
+  	$(this).removeClass('is-active');
+  });
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-
-})(jQuery);
+});
